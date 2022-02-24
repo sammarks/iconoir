3de9774e3b73fab382da5de0b45dc4e541070199
Sam Marks <sam@sammarks.me>
2022-02-24 13:43:48 -0500

    ci: switch bundling strategy

--- bin/prepublish.js
+++ bin/prepublish.js
@@ -3,7 +3,7 @@ import semver from 'semver';
 import path from 'path';
 import fs from 'fs';
 
-const BASE_VERSION = '2.1.0';
+const BASE_VERSION = '3.0.0';
 const PACKAGE_BASE = '@sammarks';
 
 function publishPackage(name) {
